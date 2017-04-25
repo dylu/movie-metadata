d315223922709ef6509ce9633f4c0dfeeb39f201
David L <davidylu@cmu.edu>
2017-04-25 04:07:50 -0400

    new functionality: adding new buttons

--- scripts/modules/filterButtons.js
+++ scripts/modules/filterButtons.js
@@ -2,6 +2,26 @@
 
 function updateButtons()
 {
-	
+
+}
+
+
+function newButton(filterString)
+{
+	var filterSplit = filterString.split(":");
+	if (filterSplit.length < 2)
+	{
+		return;
+	}
+
+	var filterType = filterSplit[0];
+	var filterValue = filterSplit[1];
+
+	d3.select("#filterButtons")
+		.append("button")
+		.attr("id", filterString)
+		.attr("type", "button")
+		// .attr("class", "classgoeshere")
+		.html("<i>" + filterType + "</i>: " + filterValue + "");
 }
 
